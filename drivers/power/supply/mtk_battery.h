c815194f0a6e72f8595aca4b252ae2ffc3b7aa7d
chailu1 <chailu1@lenovo.com>
2022-02-02 14:58:36 +0800

    charge: disable kernel battery temperature 60C poweroff
    battery temperture 68C poweroff,This is finish by framework
    So disable kernel battery 60C poweroff or reset behavior.
    
    porting from https://gerrit.mot.com/1349477
    
    Change-Id: I543441443caa2c8ec9360871cefa60acbb72263d
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2183049
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jiacheng Ou <oujc1@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2793981
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/mtk_battery.h
+++ drivers/power/supply/mtk_battery.h
@@ -879,8 +879,11 @@ struct irq_controller {
 #define SHUTDOWN_TIME 40
 #define AVGVBAT_ARRAY_SIZE 30
 #define INIT_VOLTAGE 3450
+#ifdef MTK_BASE
 #define BATTERY_SHUTDOWN_TEMPERATURE 60
-
+#else
+#define BATTERY_SHUTDOWN_TEMPERATURE 80
+#endif
 struct shutdown_condition {
 	bool is_overheat;
 	bool is_soc_zero_percent;
