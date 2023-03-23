9ab6e0e5d9c033b14c999c1db7d8203e01ce9ef8
zhu chuanchun <zhucc2@motorola.com>
2023-03-23 10:22:30 +0800

    drivers: remove unnecessary kernel_power_off()
    it should trigger by framework level if need to poweroff when the battery abnormal.
    then more debug log can output for investigate issue.
    
    Change-Id: I8d4a3b8075e64f650f257e9d2d42ebf557f47661
    Reviewed-on: https://gerrit.mot.com/2549955
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Reviewed-by: Haijian Ma <mahj8@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/2813385

--- drivers/power/supply/mtk_battery_daemon.c
+++ drivers/power/supply/mtk_battery_daemon.c
@@ -4651,7 +4651,9 @@ void sw_check_bat_plugout(struct mtk_battery *gm)
 			gm->bs_data.bat_status = POWER_SUPPLY_STATUS_UNKNOWN;
 			wakeup_fg_algo(gm, FG_INTR_BAT_PLUGOUT);
 			battery_update(gm);
+		#ifdef MTK_BASE
 			kernel_power_off();
+		#endif
 		}
 	}
 }
@@ -4860,7 +4862,9 @@ static void bat_plugout_irq_handler(struct mtk_battery *gm)
 		wakeup_fg_algo(gm, FG_INTR_BAT_PLUGOUT);
 		battery_update(gm);
 		fg_int_event(gm, EVT_INT_BAT_PLUGOUT);
+	#ifdef MTK_BASE
 		kernel_power_off();
+	#endif
 	}
 }
 
