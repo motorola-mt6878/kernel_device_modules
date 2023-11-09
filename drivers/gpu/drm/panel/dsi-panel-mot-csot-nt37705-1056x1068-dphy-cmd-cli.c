c08e42883e277138bc8c3f50bf2b509d6c4e74fc
houdz1 <houdz1@lenovo.com>
2023-11-09 16:19:16 +0800

    drm/panel: k6878v1_64 enable backlight at init code.
    The code of backlight for cli panel is not ready.
    So enable backlight at init code.
    
    Change-Id: If5422adbe057b4d002a0bb00f7a4865215a7ca93
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2790609
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2790748

--- drivers/gpu/drm/panel/dsi-panel-mot-csot-nt37705-1056x1068-dphy-cmd-cli.c
+++ drivers/gpu/drm/panel/dsi-panel-mot-csot-nt37705-1056x1068-dphy-cmd-cli.c
@@ -166,7 +166,7 @@ static void lcm_panel_init(struct lcm *ctx)
 	lcm_dcs_write_seq_static(ctx, 0x2B, 0x00, 0x00, 0x04, 0x29);
 	lcm_dcs_write_seq_static(ctx, 0x2F, 0x01);
 	lcm_dcs_write_seq_static(ctx, 0x35, 0x00);
-	lcm_dcs_write_seq_static(ctx, 0x51, 0x00, 0x00);
+	lcm_dcs_write_seq_static(ctx, 0x51, 0x08, 0xFF);
 	lcm_dcs_write_seq_static(ctx, 0x6F, 0x04);
 	lcm_dcs_write_seq_static(ctx, 0x51, 0x00, 0x00);
 	lcm_dcs_write_seq_static(ctx, 0x53, 0x20);
