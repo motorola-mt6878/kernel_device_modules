cfb9f73373d34823a25a00e15817b96852af53a8
lisn17 <lisn17@motorola.com>
2024-05-23 20:10:49 +0800

    aito: add delay for ois_reset to meet power seqence
    details:
    -add delay for ois_reset to meet power seqence in datasheet.
    -there are at least 10ms from drv_vdd to ois_reset.
    -there are 6ms in power_on.
    
    Change-Id: Icddf0010dd166f5225784cc34164c13723a012fc
    Signed-off-by: Li Suna <lisn17@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2977683
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: <huangsw2@motorola.com>
    Reviewed-by: Liu Wang <liuwang2@lenovo.com>
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/lens/ois/mot_dw9784/mot_dw9784_ois.c
+++ drivers/misc/mediatek/lens/ois/mot_dw9784/mot_dw9784_ois.c
@@ -1292,6 +1292,8 @@ static int dw9784_init(struct dw9784_device *dw9784)
 	dw_ois_mode = OIS_MODEINIT;
 
 	client->addr = DW9784_OIS_I2C_SLAVE_ADDR >> 1;
+	// there are at least 10ms from drv_vdd to ois_reset, plus 6ms in power_on.
+	ois_mdelay(4);
 	ois_reset();
 	ret = ois_i2c_rd_u16(client, 0x7011, &lock_ois);
 	LOG_INF("Check HW lock_ois: %x\n", lock_ois);
