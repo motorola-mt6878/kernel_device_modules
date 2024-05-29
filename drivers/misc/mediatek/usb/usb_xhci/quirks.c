bcb79645fad05599700b91f66b40924508a20121
liuyc20 <liuyc20@lenovo.com>
2024-05-29 11:02:48 +0800

    usb: moto usb headset(0xe57/0x0238)add to mtk_snd_quirk_flags_table
    05-27 20:14:24.383 4552 4552 E usb 1-1 : 2:0: usb_set_interface failed (-110)
    05-27 20:14:34.622 4552 4552 E usb 1-1 : 2:3: usb_set_interface failed (-110)
    
    The headset may not be able to handle this if you quickly send multiple
    control commands to the headset cable.Therefore,
    the interval for sending each control command is increased 20ms
    by QUIRK_FLAG_CTL_MSG_DELAY parameter.
    
    Change-Id: Idcf34119d438caf420b9e930b4b0b9e8b83c1300
    Reviewed-on: https://gerrit.mot.com/2983829
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/misc/mediatek/usb/usb_xhci/quirks.c
+++ drivers/misc/mediatek/usb/usb_xhci/quirks.c
@@ -37,7 +37,8 @@ static const struct usb_audio_quirk_flags_table mtk_snd_quirk_flags_table[] = {
 		   QUIRK_FLAG_CTL_MSG_DELAY),
 		DEVICE_FLG(0x04e8, 0xa051,      /* SS USBC Headset (AKG) */
 		   QUIRK_FLAG_CTL_MSG_DELAY),
-
+		DEVICE_FLG(0xe57, 0x0238,
+		   QUIRK_FLAG_CTL_MSG_DELAY),
 		{} /* terminator */
 };
 
