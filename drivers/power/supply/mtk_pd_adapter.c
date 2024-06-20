1bb4ac7331818ebb82240e2c742fdc1741a7a4ed
Haijian Ma <mahj8@motorola.com>
2024-06-20 11:44:59 +0800

    charge: Using adaptor actual data instead of 5V3A to start PPS
    As some adaptor don't has PPS 5V3A apdo, so can't enable ta charging.
    So using adaptor actual data instead of 5V3A apdo to cover all pps adaptor.
    
    
    Change-Id: I3306a74470bf23147b5fcd79dc70561e91754f6c
    Signed-off-by: Haijian Ma <mahj8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3005956
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/power/supply/mtk_pd_adapter.c
+++ drivers/power/supply/mtk_pd_adapter.c
@@ -488,6 +488,7 @@ stop_repeat:
 						 NULL, &src_cap_ext);
 	if (ret_tcp == TCP_DPM_RET_SUCCESS) {
 		data->pdp = src_cap_ext.source_pdp;
+		data->pwr_lmt = false;
 		if (data->pdp > 0 && !data->pwr_lmt)
 			data->pwr_lmt = true;
 	} else {
