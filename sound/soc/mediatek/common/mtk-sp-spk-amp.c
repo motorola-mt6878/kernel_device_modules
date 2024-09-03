4187ce7ca2c8cd4046a4bc856e06b62be8710f95
liaohj <liaohj@lenovo.com>
2024-09-03 15:57:32 +0800

    dailink:add aw882xx dailink
    milos:
    2 aw882xx smart pa
    2 analog mic 2.8v
    fsa4480 usb tyepc switch.
    
    add aw882xx dailink
    
    Change-Id: I0624f51b9b0c20d3781dbe5cd710873e156b2f9b
    Signed-off-by: liaohj <liaohj@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3076103
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/mediatek/common/mtk-sp-spk-amp.c
+++ sound/soc/mediatek/common/mtk-sp-spk-amp.c
@@ -171,7 +171,7 @@ int mtk_spk_update_info(struct snd_soc_card *card,
 	unsigned int i2s_set[2];
 	unsigned int is_ipm2p0;
 
-#if IS_ENABLED(CONFIG_SND_SOC_TFA9865)
+#if IS_ENABLED(CONFIG_SND_SOC_TFA9865) && IS_ENABLED(CONFIG_SND_SMARTPA_AW882XX)
 #else
 	if (mtk_spk_type == MTK_SPK_NOT_SMARTPA)
 		goto BYPASS_UPDATE;
@@ -305,7 +305,7 @@ int mtk_spk_update_info(struct snd_soc_card *card,
 		return -ENODEV;
 	}
 
-#if IS_ENABLED(CONFIG_SND_SOC_TFA9865)
+#if IS_ENABLED(CONFIG_SND_SOC_TFA9865) && IS_ENABLED(CONFIG_SND_SMARTPA_AW882XX)
 #else
 BYPASS_UPDATE:
 #endif
