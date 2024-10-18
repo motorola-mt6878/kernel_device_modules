ced68638962f6ab4c39a69b8e0747634b187b018
Simon C <chenzm8@motorola.com>
2024-10-18 19:20:42 +0800

    scout: Fix 2nd pmic probe failed issue
    Details:
      Add match rule for 2nd pmic
    
    Change-Id: If41a517c8a637cd0ea1d2aa2157311601951d717
    Signed-off-by: Simon C <chenzm8@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3117131
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- drivers/regulator/aw370041c-regulator.c
+++ drivers/regulator/aw370041c-regulator.c
@@ -51,6 +51,7 @@
 
 #define  AW37004_ID                             0x00
 #define  AW37004_ID2                            0x04
+#define  WL28661D_CHIP_ID                       0x33
 
 static int ldo_chipid = -1;
 
@@ -382,19 +383,30 @@ static int aw37004_i2c_probe(struct i2c_client *client,
 	}
 
 	ret = regmap_read(chip->regmap, AW37004_CHIP_REV, &ldo_chipid);
-	if (ret < 0 || ldo_chipid != AW37004_ID) {
-		dev_err(dev, "Failed to read CHIP ID:0x%x, ret:%d\n", ldo_chipid,ret);
-		ret = -ENODEV;
+	if (ret < 0){
+    		dev_err(dev, "Failed to read CHIP ID:0x%x, ret:%d\n", ldo_chipid,ret);
+    		ret = -ENODEV;
 		return ret;
-	} else {
-		dev_info(chip->dev, "AW37004 CHIP ID matched!\n");
-	}
+  	} else{
+    		switch(ldo_chipid){
+      			case AW37004_ID:
+          			dev_info(chip->dev, "AW370041C CHIP ID matched!\n");
+          			break;
+      			case WL28661D_CHIP_ID:
+          			dev_info(chip->dev, "WL28661D1C CHIP ID matched!\n");
+          			break;
+      			default:
+          			dev_err(dev, "CHIP ID not mactched:0x%x, maybe other ic, ret:%d\n", ldo_chipid,ret);
+          			ret = -ENODEV;
+          			return ret;
+    		}
+  	}
 
 	ret = regmap_read(chip->regmap, AW37004_CHIP_REV2, &ldo_chipid);
 	if (ret < 0 || ldo_chipid != AW37004_ID2) {
-		dev_err(dev, "ET5904 matched, read CHIP ID2:0x%x, ret:%d\n", ldo_chipid,ret);
+		dev_err(dev, "CHIP ID2 not mactched:0x%x, maybe other ic, ret:%d\n", ldo_chipid,ret);
 	} else {
-		dev_info(chip->dev, "AW37004 CHIP ID2 matched!\n");
+		dev_info(chip->dev, "AW370041C CHIP ID2 matched!\n");
 	}
 
 	for (i = 0; i < 5; i++) {
