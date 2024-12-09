bbab81e8d2c1b5dab92fc1005d42b6e1c15721c8
zhaoxp3 <zhaoxp3@motorola.com>
2024-12-09 10:05:03 +0800

    Android.mk: fix for kernel version time
    Set SOURCE_DATE_EPOCH as 0 to avoid that .git folder
    is not completed.
    
    Change-Id: I606a7e1974e2fbb9da3135e22e14f3199087834a
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3165564
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- Android.mk
+++ Android.mk
@@ -75,6 +75,8 @@ $(KERNEL_ZIMAGE_OUT): PRIVATE_BAZEL_BUILD_FLAG += --config=local
 else
 ifneq (,$(wildcard kernel/$(REL_ACK_DIR)/.git))
 $(KERNEL_ZIMAGE_OUT): PRIVATE_BAZEL_BUILD_FLAG += --config=stamp --repo_manifest=$(abspath $(KERNEL_DIR)/fake_manifest.xml)
+endif
+ifeq (,$(wildcard kernel/$(REL_ACK_DIR)/.git))
 $(KERNEL_ZIMAGE_OUT): PRIVATE_BAZEL_EXPORT_ENV += SOURCE_DATE_EPOCH=0
 endif
 endif
