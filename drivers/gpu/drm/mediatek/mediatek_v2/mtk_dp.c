d586db0d4f21b981dd5ad6a1694dc4cc4b7f1d28
lalit.w <lalit.w@mediatek.com>
2025-03-22 05:42:55 +0800

    [ALPS09849862] disp: dp: Refine DPTX power on flow
    [Description]
    modify sleep handling during DPTX init flow.
    This will also remove disconnect and IRQ delay.
    
    [Test]
    connect DP ok
    
    MTK-Commit-Id: 335d3b5823c7a9f9e90959f79ccf51c48338f6ac
    
    Change-Id: Idbb4046d908e520806700d9275f7780dccd4c6f3
    Signed-off-by: lalit.w <lalit.w@mediatek.com>
    CR-Id: ALPS09849862
    Feature: display_mobile

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp.c
@@ -4039,14 +4039,13 @@ void mtk_dp_HPDInterruptSet(int bstatus)
 	DPTXMSG("%s, status:%d[2:DISCONNECT, 4:CONNECT, 8:IRQ] Power:%d, uevent=%d\n",
 		__func__, bstatus, g_mtk_dp->bPowerOn, g_mtk_dp->bUeventToHwc);
 
-	// delay to prevent from slow connecting
-	msleep(500);
-
 	if ((bstatus == HPD_CONNECT && !g_mtk_dp->bPowerOn)
 		|| (bstatus == HPD_DISCONNECT && g_mtk_dp->bPowerOn)
 		|| (bstatus == HPD_INT_EVNET && g_mtk_dp->bPowerOn)) {
 
 		if (bstatus == HPD_CONNECT) {
+			// delay to prevent from slow connecting
+			msleep(500);
 			if (g_mtk_dp->priv->data->mmsys_id == MMSYS_MT6989) {
 				if (g_mtk_dp->priv->dpc_dev) {
 					/* get mminfra before DPTX on */
