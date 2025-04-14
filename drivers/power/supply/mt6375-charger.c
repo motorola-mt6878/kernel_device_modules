26ee52c25894c3cd167e07f9fdb8b81921563575
Yifan Xu <xuyf4@motorola.com>
2025-04-14 19:19:41 +0800

    reduce uevent for moto design
    too many uevent in moto design
    not send uevent when info not changed.
    
    Change-Id: I5e82151db1f6670e26b4ff03cfc0205132dc2d4c
    Reviewed-on: https://gerrit.mot.com/3348399
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wei Xu <xuwei9@lenovo.com>
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3539285
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Ravindranath Sattaru <sravindr@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3660859

--- drivers/power/supply/mt6375-charger.c
+++ drivers/power/supply/mt6375-charger.c
@@ -1088,11 +1088,58 @@ static void mt6375_chg_attach_pre_process(struct mt6375_chg_data *ddata,
 		dev_notice(ddata->dev, "%s bc12 work already queued\n", __func__);
 }
 
+static int mt6375_get_vbus(struct charger_device *chgdev, u32 *vbus);
+static bool mmi_is_vbus_changed(struct mt6375_chg_data *ddata)
+{
+	static bool g_vbus_present = false;
+	int vbus_uv = 0;
+	bool vbus_present = false;
+	bool vbus_changed = false;
+	int rc;
+
+	if (IS_ERR_OR_NULL(ddata->chgdev))
+		return false;
+
+	rc = mt6375_get_vbus(ddata->chgdev, &vbus_uv);
+	if (rc != 0) {
+		pr_err("%s get vbus failed\n",__func__);
+		return false;
+	}
+	if (vbus_uv >= 3000000)
+		vbus_present = true;
+
+	if (g_vbus_present == vbus_present) {
+		vbus_changed = false;
+	} else {
+		vbus_changed = true;
+	}
+	g_vbus_present = vbus_present;
+	dev_info(ddata->dev, "%s, vbus:%d, vbus_changed:%d\n",__func__, vbus_uv, vbus_changed);
+
+	return vbus_changed;
+}
+
 #define CHG_SHOW_MAX_SIEZE 50
 static int mmi_notify_vbus_event(struct mt6375_chg_data *ddata, bool vbus_status)
 {
 	char *event_string = NULL;
 	union power_supply_propval val = {0};
+	struct power_supply *chg_psy = NULL;
+	struct mtk_charger *info = NULL;
+	bool water_detected = false;
+
+	chg_psy = power_supply_get_by_name("mtk-master-charger");
+	if (IS_ERR_OR_NULL(chg_psy)) {
+		dev_info(ddata->dev, "%s Couldn't get chg_psy\n", __func__);
+		return -EINVAL;
+	} else {
+		info = (struct mtk_charger *)power_supply_get_drvdata(chg_psy);
+		if (info == NULL) {
+			dev_info(ddata->dev, "%s info is NULL\n", __func__);
+			return -EINVAL;
+		}
+		water_detected = info->water_detected;
+	}
 
 	if(!ddata->batt_psy)
 		ddata->batt_psy = power_supply_get_by_name("battery");
@@ -1112,16 +1159,18 @@ static int mmi_notify_vbus_event(struct mt6375_chg_data *ddata, bool vbus_status
 			return 0;
 	}
 
-	event_string = kmalloc(CHG_SHOW_MAX_SIEZE, GFP_KERNEL);
+	if (mmi_is_vbus_changed(ddata) && water_detected) {
+		event_string = kmalloc(CHG_SHOW_MAX_SIEZE, GFP_KERNEL);
 
-	scnprintf(event_string, CHG_SHOW_MAX_SIEZE,
-			"POWER_SUPPLY_VBUS_PRESENT=%s", vbus_status? "true": "false");
+		scnprintf(event_string, CHG_SHOW_MAX_SIEZE,
+				"POWER_SUPPLY_VBUS_PRESENT=%s", vbus_status? "true": "false");
 
-	ddata->batt_uenvp[0] = event_string;
-	ddata->batt_uenvp[1] = NULL;
-	kobject_uevent_env(&ddata->batt_psy->dev.kobj, KOBJ_CHANGE, ddata->batt_uenvp);
-	dev_info(ddata->dev, "%s, vbus:%d send %s\n",__func__, vbus_status, event_string);
-	kfree(event_string);
+		ddata->batt_uenvp[0] = event_string;
+		ddata->batt_uenvp[1] = NULL;
+		kobject_uevent_env(&ddata->batt_psy->dev.kobj, KOBJ_CHANGE, ddata->batt_uenvp);
+		dev_info(ddata->dev, "%s, vbus:%d send %s\n",__func__, vbus_status, event_string);
+		kfree(event_string);
+	}
 	return 0;
 }
 
